521ecf95751c2783cd4e81c8bd8f7d9e1375cd55
BRF2002 <101672998+BRF2002@users.noreply.github.com>
2022-03-29 22:15:04 -0300

    Add files via upload

---
+++ Pr-tica3_OO/src/ex1/Normal.java
@@ -0,0 +1,11 @@
+package ex1;
+
+public class Normal extends Ingresso{
+
+	Normal(double valor) {
+		super(valor);
+	}
+	public void imprimeIngresso(){
+		System.out.println("Ingresso Normal valor "+ getValor());
+}
+}
