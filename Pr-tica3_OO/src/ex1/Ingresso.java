705111aa07fce82fe5cec7639b7a70f30f2f544b
BRF2002 <101672998+BRF2002@users.noreply.github.com>
2022-03-29 22:18:19 -0300

    Add files via upload

--- Pr-tica3_OO/src/ex1/Ingresso.java
+++ Pr-tica3_OO/src/ex1/Ingresso.java
@@ -2,4 +2,16 @@ package ex1;
 
 public class Ingresso {
 
+	private double valor;
+
+	public Ingresso(double valor) {
+		this.valor = valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	public double getValor() {
+		return valor;
+	}
+
 }
